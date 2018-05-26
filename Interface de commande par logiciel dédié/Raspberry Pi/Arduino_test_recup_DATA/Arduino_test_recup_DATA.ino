f507f8402abebdb53052596e9cc924b192bf3958
lounes-kmt <38882571+lounes-kmt@users.noreply.github.com>
2018-05-26 18:33:58 +0400

    Add arduino test script

---
+++ Interface de commande par logiciel dédié/Raspberry Pi/Arduino_test_recup_DATA/Arduino_test_recup_DATA.ino
@@ -0,0 +1,40 @@
+#include <time.h>
+
+char datamess[100] = "";
+int heure = 15;
+int minute = 12;
+int sec = 0;
+int count1 = 0;
+int count2 = 0;
+int coinc = 0;
+int temps = 0;
+int tempsData = 0;
+
+void setup() 
+{
+  Serial.begin(9600);
+
+  temps = millis();
+  tempsData = millis();
+}
+
+void loop() {
+  count1 = rand() % 4;
+  count2 = rand() % 4;
+  coinc += rand() % 3 + 1;
+    
+  sprintf(datamess, "DATA 2018, 05, 26, %d, %d, %d, 99, (*), 6000, -999.0, -999.0,  -999.0, 33.2, 101000, 0, %d, %d, %d", heure, minute, sec, count1, count2, coinc);
+  Serial.println(datamess);
+
+  sec += 5;
+  if(sec == 60) {
+    sec = 0;
+    minute++;
+  }
+  if(minute == 60) {
+    minute = 0;
+    heure++;
+  }
+  delay(5000);
+}
+
